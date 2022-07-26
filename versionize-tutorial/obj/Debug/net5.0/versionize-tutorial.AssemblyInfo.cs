5cee7ed3c3f8119c3b53722132b77f7dbcd0818d
Sashi Kumar <sashi@inchz-iot.com>
2022-07-26 12:50:12 +0800

    updates

--- versionize-tutorial/obj/Debug/net5.0/versionize-tutorial.AssemblyInfo.cs
+++ versionize-tutorial/obj/Debug/net5.0/versionize-tutorial.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("versionize-tutorial")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.1.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.1.0-alpha.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.1.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("versionize-tutorial")]
 [assembly: System.Reflection.AssemblyTitleAttribute("versionize-tutorial")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.1.0.0")]
