b4f7eb0d1718d52994f399c3de539a1f67280189
Sashi Kumar <sashi@inchz-iot.com>
2022-07-27 11:36:14 +0800

    fix: 100000

--- versionize-tutorial/Program.cs
+++ versionize-tutorial/Program.cs
@@ -15,6 +15,8 @@ namespace versionize_tutorial
             Console.WriteLine("Test7");
             Console.WriteLine("Test8");
             Console.WriteLine("Test9");
+            Console.WriteLine("Test10");
+
 
 
 
