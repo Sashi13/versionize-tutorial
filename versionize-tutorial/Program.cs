3da57ae21639f2c845aebb698a8a616feea418fa
Sashi Kumar <sashi@inchz-iot.com>
2022-07-27 16:09:33 +0800

    chore!: huge update

--- versionize-tutorial/Program.cs
+++ versionize-tutorial/Program.cs
@@ -30,6 +30,8 @@ namespace versionize_tutorial
             Console.WriteLine("Test22");
             Console.WriteLine("Test23");
             Console.WriteLine("Test24 - test branch");
+            Console.WriteLine("Test25");
+
         }
     }
 }
